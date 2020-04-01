53d2fd41348f1922d2bb274fae3fd5a5a79507be
Sujith <sujith@Sujiths-MacBook-Pro.local>
2020-04-01 13:24:17 +0530

    removed client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const schema = require("./schema");
+const schema = require("./app/schema");
 const mongoose = require("mongoose");
 
 mongoose.connect(
